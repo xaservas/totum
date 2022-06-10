b6abdf41398b1effffad24e92fffd5428a0daf39
kenny caldieraro <kenny.caldieraro@gmail.com>
2022-06-10 19:32:26 +0200

    fix datamapper && controller activity

---
+++ api/app/controllers/api/activitycontroller.js
@@ -0,0 +1,49 @@
+const activityDataMapper = require('../../models/activityDataMapper');
+
+const activityController = {
+    async getAll(_, res) {
+        const activities = await activityDataMapper.getAll();
+        return res.json(activities);
+    },
+
+    async getByCategory(req, res) {
+        const activities = await activityDataMapper.getByCategory(req.params.category);
+        if (!activities) {
+            return res.status(404).json({ message: 'No activities found' });
+        }
+        return res.json(activities);
+    },
+
+    async getByGeo(req, res) {
+        const activities = await activityDataMapper.getByGeo(req.params.geo);
+        return res.json(activities);
+    },
+
+    async getBySearch(req, res) {
+        const activities = await activityDataMapper.getBySearch(req.params.search);
+        return res.json(activities);
+    },
+
+    async getOneActivity(req, res) {
+        const activities = await activityDataMapper.getOneActivity(req.params.id);
+        return res.json(activities);
+    },
+
+    async createActivity(req, res) {
+        const activities = await activityDataMapper.createActivity(req.body);
+        return res.json(activities);
+    },
+
+    async updateActivity(req, res) {
+        const activities = await activityDataMapper.updateActivity(req.params.id, req.body);
+        return res.json(activities);
+    },
+
+    async removeActivity(req, res) {
+        const activities = await activityDataMapper.removeActivity(req.params.id);
+        return res.json(activities);
+    },
+
+};
+
+module.exports = activityController;
---
+++ api/app/models/activityDataMapper.js
@@ -0,0 +1,124 @@
+const client = require('./client');
+
+const activityDataMapper = {
+
+    async getAll() {
+        const query = {
+            text: 'SELECT * FROM activity',
+            values: [],
+        };
+        const result = await client.query(query);
+        return result.rows;
+    },
+
+    async getByCategory(idCategory) {
+        const query = {
+            text: ' SELECT * FROM activity WHERE id_category = $1 ',
+            values: [idCategory],
+        };
+        const result = await client.query(query);
+        if (result.rows.length === 0) {
+            return null;
+        }
+        return result.rows;
+    },
+
+    async getByGeo(idGeolocalisation) {
+        const query = {
+            text: ' SELECT * FROM activity WHERE landmark = $1 ',
+            values: [idGeolocalisation],
+        };
+        const result = await client.query(query);
+        if (result.rows.length === 0) {
+            return 'No activity found';
+        }
+        return result.rows;
+    },
+
+    async getBySearch(idKeyWord) {
+        console.log(idKeyWord);
+        const query = {
+            text: ' SELECT * FROM activity WHERE name LIKE \'% $1 %\' ',
+            values: [idKeyWord],
+        };
+        const result = await client.query(query);
+        if (result.rows.length === 0) {
+            return 'No results found';
+        }
+        return result.rows;
+    },
+
+    async getOneActivity(idActivity) {
+        const query = {
+            text: 'SELECT * FROM activity WHERE id = $1',
+            values: [idActivity],
+        };
+        const result = await client.query(query);
+        if (!result.rows[0]) {
+            throw new Error('Activity not found');
+        }
+        return result.rows;
+    },
+
+    async createActivity(data) {
+        const query = {
+            text: 'INSERT INTO activity (name, level, address, zip_code, description, date, city, country, landmark, id_user, id_category) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11) RETURNING *',
+            values: [
+                data.name,
+                data.level,
+                data.address,
+                data.zip_code,
+                data.description,
+                data.date,
+                data.city,
+                data.country,
+                data.landmark,
+                data.id_user,
+                data.id_category,
+            ],
+
+        };
+        const result = await client.query(query);
+        return result.rows[0];
+    },
+
+    async updateActivity(id, data) {
+        const query = {
+            text: 'UPDATE activity SET name = $1, level = $2, address = $3, zip_code = $4, description = $5, date = $6, city = $7, country = $8, landmark = $9, id_user = $10, id_category = $11 WHERE id = $12 RETURNING *',
+            values: [
+                data.name,
+                data.level,
+                data.address,
+                data.zip_code,
+                data.description,
+                data.date,
+                data.city,
+                data.country,
+                data.landmark,
+                data.id_user,
+                data.id_category,
+                id,
+            ],
+        };
+        const result = await client.query(query);
+        if (!result.rows[0]) {
+            throw new Error('Activity not found');
+        }
+        return result.rows[0];
+    },
+
+    async removeActivity(id) {
+        const query = {
+            text: 'DELETE FROM activity WHERE id = $1 RETURNING *',
+            values: [id],
+        };
+        const result = await client.query(query);
+        if (!result.rows[0]) {
+            throw new Error('Activity not found');
+        }
+        return result.rows[0];
+    },
+
+};
+
+module.exports = activityDataMapper;
