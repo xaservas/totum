3b442a279eecd83eac268c0280c14309d5f41746
kenny caldieraro <kenny.caldieraro@gmail.com>
2022-06-10 15:48:03 +0200

    fix error syntax

--- api/app/validation/schemas/userManage.schema.js
+++ api/app/validation/schemas/userManage.schema.js
@@ -3,6 +3,7 @@ const Joi = require('joi');
 const textRule = Joi.string().min(2).pattern(/^[a-zA-ZÀ-ÿ0-9 ']+$/);
 const numberRule = Joi.number().min(0);
 const emailRule = Joi.string().email();
+const meta = Joi.boolean();
 
 module.exports = Joi.object({
     email: emailRule,
@@ -16,4 +17,6 @@ module.exports = Joi.object({
     city: textRule,
     country: textRule,
     meta_id: numberRule,
+    cookie: meta,
+    landmark: meta,
 });
