e1d8c553c8ea293b14696ee3b35d0ef4db5715fa
kenny caldieraro <kenny.caldieraro@gmail.com>
2022-06-12 15:35:48 +0200

    add all new joi schema

---
+++ api/app/validation/schemas/category/categoryManage.schema.js
@@ -0,0 +1,12 @@
+const Joi = require('joi');
+
+const textRule = Joi.string().min(2).pattern(/^[a-zA-ZÀ-ÿ0-9 ']+$/);
+const numberRule = Joi.number().min(0);
+const pictoRule = Joi.string().min(2);
+
+module.exports = Joi.object({
+    id: numberRule,
+    name: textRule,
+    picto: pictoRule,
+    id_user: numberRule,
+});
