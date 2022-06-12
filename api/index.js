8ed7e4c9d25093e0a53b8d225d31911da39d5790
kenny caldieraro <kenny.caldieraro@gmail.com>
2022-06-12 18:24:35 +0200

    add swagger / jsdoc

--- api/index.js
+++ api/index.js
@@ -18,17 +18,10 @@ const options = {
         title: 'Totum',
     },
     baseDir: __dirname,
-    // Glob pattern to find your jsdoc files (multiple patterns can be added in an array)
     filesPattern: './**/*.js',
-    // URL where SwaggerUI will be rendered
     swaggerUIPath: '/api-docs',
-    // Expose OpenAPI UI
     exposeSwaggerUI: true,
-    // Set non-required fields as nullable by default
     notRequiredAsNullable: false,
-    // You can customize your UI options.
-    // you can extend swagger-ui-express config. You can checkout an example of this
-    // in the `example/configuration/swaggerOptions.js`
     swaggerUiOptions: {},
 };
 
