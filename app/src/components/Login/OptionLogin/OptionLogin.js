89032638af638411e3fb00dc5f4e3fbc528ef13e
DamienCHERON <cherondamien@dc-web.fr>
2022-06-12 21:10:25 +0200

    pagelogin + router

---
+++ app/src/components/Login/OptionLogin/OptionLogin.js
@@ -0,0 +1,23 @@
+
+
+import './OptionLogin.scss';
+
+function OptionLogin(){
+        return (
+            <div className="OptionLogin">
+            <label className="checkbox">
+                <input type="checkbox"/>
+                <span className="slider round"></span>
+                <p>Géolocalisation</p>
+            </label>
+            <label className="checkbox">
+                <input type="checkbox"/>
+                <span className="slider round"></span>
+                <p>Coockies</p>
+            </label>
+            
+        </div>
+        );
+};
+
+export default OptionLogin;
