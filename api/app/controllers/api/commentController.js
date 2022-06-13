aa11777907fbd7bb3c70640def41bc28bc500b15
anaselidrissi21 <aei21460@gmail.com>
2022-06-13 13:37:21 +0200

    controller comments and category

---
+++ api/app/controllers/api/commentController.js
@@ -0,0 +1,41 @@
+const commentDataMapper = require('../../models/categoryDatamapper');
+const { createComment, getOneComment, removeComment, getByUser, getByActivity } = require('../../models/commentDatamapper');
+
+commentController = {
+
+    async createComment(req, res) {
+        const comments = await commentDataMapper.createComment(req.body);
+        return res.json(comments);
+    },
+
+    async getOneComment(req, res) {
+        const comments = await commentDataMapper.getOneComment(req.params.id);
+        return res.json(comments);
+    },
+
+    async updateComment(req, res) {
+        const comments = await commentDataMapper.updateComment(req.params.id, req.body);
+        return res.json(comments);
+    },
+
+    async removeComment(req, res) {
+        const comments = await commentDataMapper.removeComment(req.params.id);
+        return res.json(comments);
+    },
+
+    async getByUser(req, res) {
+        const comments = await commentDataMapper.getByUser(req.params.id);
+        return res.json(comments);
+    },
+
+    async getByActivity(req, res) {
+        const comments = await commentDataMapper.getByActivity(req.params.id);
+        return res.json(comments);
+    },
+
+
+
+
+};
+
+module.exports = commentController
