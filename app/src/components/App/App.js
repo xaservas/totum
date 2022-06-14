fbf647d5d145099c371d89aedcd80da5ec7ce5a5
DamienCHERON <cherondamien@dc-web.fr>
2022-06-14 15:59:11 +0200

    maj_token

--- app/src/components/App/App.js
+++ app/src/components/App/App.js
@@ -1,17 +1,21 @@
+import React from "react";
+
 import Header from "../Header/Header";
-import Profile from "../Profile/Profile";
-import Activity from "../Activity/Activity";
-import ListActivities from "../ListActivities/ListActivities";
+// import Profile from "../Profile/Profile";
+// import Activity from "../Activity/Activity";
+// import ListActivities from "../ListActivities/ListActivities";
 import { Route, Routes } from 'react-router-dom';
 import Login from "../Login/Login";
 
-import activities from "../../data/activities"
-import CreateActivity from "../CreateActivity/CreateActivity";
-import CreateProfile from "../CreateProfil/CreateProfile";
+// import activities from "../../data/activities"
+// import CreateActivity from "../CreateActivity/CreateActivity";
+// import CreateProfile from "../CreateProfil/CreateProfile";
 
 
 
 function App() {
+ 
+
   return (
     <div className="App">
       
@@ -19,11 +23,11 @@ function App() {
     <Routes>
       <Route path="/" element={<Login/>}></Route>
 
-      <Route path="/activity/:id" element={<Activity activities={activities}/>}></Route>
-      <Route path="/profile" element={<Profile activities={activities}/>}></Route>
-      <Route path="/activities" element={<ListActivities activities={activities}/>}></Route>
-      <Route path="/activity/create" element={<CreateActivity/>}></Route>
-      <Route path="/createProfil" element={<CreateProfile/>}></Route>
+      {/* <Route path="/activity/:id" element={<Activity activities={activities}/>}></Route> */}
+      {/* <Route path="/profile" element={<Profile activities={activities}/>}></Route> */}
+      {/* <Route path="/activities" element={<ListActivities activities={activities}/>}></Route> */}
+      {/* <Route path="/activity/create" element={<CreateActivity/>}></Route> */}
+      {/* <Route path="/createProfil" element={<CreateProfile/>}></Route> */}
 
     </Routes>
         
