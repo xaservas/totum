edd94c65c6b54c0105a1df2a032ec16beb4edc74
kenny caldieraro <kenny.caldieraro@gmail.com>
2022-06-14 13:52:09 +0200

    fix bug & return error

--- api/app/controllers/api/commentController.js
+++ api/app/controllers/api/commentController.js
@@ -1,14 +1,24 @@
-const commentDataMapper = require('../../models/categoryDatamapper');
+const commentDataMapper = require('../../models/commentDatamapper');
 
 const commentController = {
 
     async getByUser(req, res) {
         const comments = await commentDataMapper.getByUser(req.params.id);
+        if (comments.length === 0) {
+            return res.status(404).json({
+                error: 'No comments found',
+            });
+        }
         return res.json(comments);
     },
 
     async getByActivity(req, res) {
         const comments = await commentDataMapper.getByActivity(req.params.id);
+        if (comments.length === 0) {
+            return res.status(404).json({
+                error: 'No comments found',
+            });
+        }
         return res.json(comments);
     },
 
