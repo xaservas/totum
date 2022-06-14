8a8c8bf1183e5784cbda0641aaa33aa1c5962c78
TENETbenoit <benoittenet@gmail.com>
2022-06-14 14:55:29 +0200

    header-totum

--- app/src/components/Header/Header.js
+++ app/src/components/Header/Header.js
@@ -1,11 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './header.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee, faUser  } from '@fortawesome/free-solid-svg-icons'
+
+/*
+HELLO
+, je sais pas si tu es dans le coin mais pour gérer le responsive avec bulma je crois que j'ai trouvé un truc:
+https://bulma.io/documentation/overview/responsiveness/
+J'ai rajouté la class "is-mobile" à la navbar, c'est censé forcer le tout à s'afficher horizontalement, check si c'est bon! 
+Xav
+*/
 
 function Header({...rest}){
    return (
-       <header>           
-           <p>header</p>
+       <header>  
+        <nav class="navbar" role="navigation" aria-label="main navigation">
+         <div  class="navbar-brand title is large">
+            <div className='barre'>
+                <div className='title'>
+                TOTUM 
+                </div>
+                <div className='icon'>
+                <FontAwesomeIcon icon={faUser} className='navbar-item ' />
+
+                </div>
+            </div>
+            
+          
+         </div>
+        </nav>
+        
         </header>
    );
 };
