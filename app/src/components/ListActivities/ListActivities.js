bea27856fddb78ce5026031358ab8947fc64b10a
Xavier Servas <xavier.servas@gmail.com>
2022-06-15 13:33:50 +0200

    activitiesDataRequest success

--- app/src/components/ListActivities/ListActivities.js
+++ app/src/components/ListActivities/ListActivities.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import './listActivities.scss';
+import axios from '../../utils/axiosPool';
 
 function ListActivities({
     list_type,
-    activities, 
+    /*activities, */
     ...rest}){
+        const [activities, setActivities] = useState([]);
+
+        const ActivitiesDataRequest = async () => {
+            try {
+                const result= await axios({
+                    method:'get',
+                    url: '/activities'
+                })
+                console.log(result.data); 
+                setActivities(result.data) ;
+            } catch (error) {
+                console.log(error)
+            }
+        };
+
+        useEffect(() => {
+            ActivitiesDataRequest()
+        },[]);
+
    return (
        <article
            className={'listActivities panel'}
@@ -31,12 +51,13 @@ function ListActivities({
 ListActivities.propTypes = {
     className: PropTypes.string,
     list_type: PropTypes.string,
-    activities: PropTypes.arrayOf(PropTypes.shape({
+    
+    /*activities: PropTypes.arrayOf(PropTypes.shape({
         id: PropTypes.number.isRequired,
         name: PropTypes.string.isRequired,
         level: PropTypes.string.isRequired,
         tag: PropTypes.string.isRequired,
-    }).isRequired).isRequired,
+    }).isRequired).isRequired,*/
 };
         
 ListActivities.defaultProps = {
