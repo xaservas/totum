95134648c65ebdf269857b44823bb21b0c057726
kenny caldieraro <kenny.caldieraro@gmail.com>
2022-06-16 21:40:25 +0200

    fix rules lint

--- api/.eslintrc.js
+++ api/.eslintrc.js
@@ -1,23 +1,22 @@
 module.exports = {
-    env: {
-        commonjs: true,
-        es2021: true,
-        node: true,
-        'jest/globals': true,
-    },
-    extends: [
-        'airbnb-base',
-    ],
-    plugins: ['jest'],
-    parserOptions: {
-        ecmaVersion: 'latest',
-    },
-    rules: {
-        indent: ['error', 4],
-        'jest/no-disabled-tests': 'warn',
-        'jest/no-focused-tests': 'error',
-        'jest/no-identical-title': 'error',
-        'jest/prefer-to-have-length': 'warn',
-        'jest/valid-expect': 'error',
-    },
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+    'jest/globals': true,
+  },
+  extends: ['airbnb-base'],
+  plugins: ['jest'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    indent: ['error', 4],
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/prefer-to-have-length': 'warn',
+    'jest/valid-expect': 'error',
+    quotes: ['error', 'simple'],
+  },
 };
