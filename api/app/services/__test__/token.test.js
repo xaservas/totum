d696cc4ee83f01aa9d3156d1812ed67a52fe1971
kenny caldieraro <kenny.caldieraro@gmail.com>
2022-06-16 12:08:29 +0200

    update TU token

--- api/app/services/__test__/token.test.js
+++ api/app/services/__test__/token.test.js
@@ -1,11 +1,33 @@
 const { mockRequest, mockResponse } = require('../interceptor');
 const tokenController = require('../token');
+const userDataMapper = require('../../models/userDatamapper');
+
+function generateToken() {
+    const data = 'test@test.com';
+    const token = tokenController.generateToken(data);
+    return token;
+}
 
 describe('Check token controllers', () => {
     test('Check if token is generated with email', () => {
-        const data = 'test@test.com';
+        const tokenLength = generateToken().length;
+        expect(generateToken()).toBeDefined();
+        expect(generateToken()).toBeTruthy();
+        expect(tokenLength).toBeGreaterThan(100);
+        expect(tokenLength).toBeLessThan(200);
+        expect(generateToken()).toMatch(/^[a-zA-Z0-9-_]+\.[a-zA-Z0-9-_]+\.[a-zA-Z0-9-_]+$/);
+    });
 
-        expect(tokenController.generateToken(data)).toBeDefined();
-        expect(tokenController.generateToken(data)).toBeTruthy();
+    test('Check if token is not blacklisted', async () => {
+        const result = await tokenController.blacklistToken(generateToken());
+        expect(result).toBeUndefined();
+    });
+
+    test('Check if token is blacklisted', async () => {
+        const token = generateToken();
+        await userDataMapper.logout(token);
+        const result = await tokenController.blacklistToken(token);
+        expect(result).toBeDefined();
+        expect(result.token).toBe(token);
     });
 });
