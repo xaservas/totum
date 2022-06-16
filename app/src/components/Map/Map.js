06a6e31cae988a9bfad1099b6344ef802b1d5eb8
kenny caldieraro <kenny.caldieraro@gmail.com>
2022-06-16 21:42:12 +0200

    add map

--- app/src/components/Map/Map.js
+++ app/src/components/Map/Map.js
@@ -1,26 +1,39 @@
 import { Marker, Popup } from 'leaflet';
-import { MapContainer, TileLayer, useMap } from 'react-leaflet'
+import { MapContainer, TileLayer, useMap } from 'react-leaflet';
 import './map.scss';
 
-
 function Map() {
+  return (
+    <div className='Map'>
+      <link
+        rel='stylesheet'
+        href='https://unpkg.com/leaflet@1.8.0/dist/leaflet.css'
+        integrity='sha512-hoalWLoI8r4UszCkZ5kL8vayOGVae1oxXe/2A4AO6J9+580uKHDO3JdHb7NzwwzK5xr/Fs0W40kiNHxM9vyTtQ=='
+        crossOrigin=''
+      />
+      <script
+        src='https://unpkg.com/leaflet@1.8.0/dist/leaflet.js'
+        integrity='sha512-BB3hKbKWOc9Ez/TAwyWxNXeoV9c1v6FIeYiBieIWkpLjauysF18NzgR1MBNBXf8/KABdlkX68nAhlwcDFLGPCQ=='
+        crossOrigin=''
+      />
 
-return (
+      <MapContainer
+        center={[42.6886591, 2.8948332]}
+        zoom={13}
+        scrollWheelZoom={false}>
+        <TileLayer
+          attribution='OpenStreetMap'
+          url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+        />
 
-  <div className="Map">
-     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.8.0/dist/leaflet.css"
-   integrity="sha512-hoalWLoI8r4UszCkZ5kL8vayOGVae1oxXe/2A4AO6J9+580uKHDO3JdHb7NzwwzK5xr/Fs0W40kiNHxM9vyTtQ=="
-   crossorigin=""/>
-<MapContainer center={[42.6886591, 2.8948332]} zoom={13} scrollWheelZoom={false}>
-  <TileLayer
-    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-  />
-
-</MapContainer>
-  </div>
-
-)
+        {/* <Marker position={[42.6886591, 2.8948332]}>
+          <Popup>
+            <span>Totum #13</span>
+          </Popup>
+        </Marker> */}
+      </MapContainer>
+    </div>
+  );
 }
 
 export default Map;
