7320f9f36c2e3ff276940e754b0796e92f732d09
kenny caldieraro <kenny.caldieraro@gmail.com>
2022-06-17 20:04:40 +0200

    update nav

--- app/src/components/Header/Header.js
+++ app/src/components/Header/Header.js
@@ -1,28 +1,32 @@
-import React from 'react';
 import PropTypes from 'prop-types';
 import './header.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
 import { useNavigate } from 'react-router-dom';
-/*
-HELLO
-, je sais pas si tu es dans le coin mais pour gérer le responsive avec bulma je crois que j'ai trouvé un truc:
-https://bulma.io/documentation/overview/responsiveness/
-J'ai rajouté la class "is-mobile" à la navbar, c'est censé forcer le tout à s'afficher horizontalement, check si c'est bon!
-Xav
-*/
+import Axios from '../../utils/axiosPool';
 
-function Header({ ...rest }) {
+function Header() {
   const navigate = useNavigate();
 
-  const onClick = async (event) => {
+  const showMenu = (event) => {
+    event.preventDefault();
+    const menu = document.getElementById('navbar-menu');
+    menu.classList.toggle('showMenu');
+  };
+
+  const backToHome = async (event) => {
     event.preventDefault();
     navigate('/', { replace: true });
   };
 
-  const onClickLogout = async (event) => {
+  const logout = async (event) => {
     event.preventDefault();
-    localStorage.removeItem('token');
+    localStorage.clear();
+    try {
+      await Axios.get('/user/logout');
+    } catch (error) {
+      console.log(error);
+    }
     navigate('/login', { replace: true });
   };
 
@@ -36,19 +40,13 @@ function Header({ ...rest }) {
     navigate('/profil/params', { replace: true });
   };
 
-  const showMenu = (event) => {
-    event.preventDefault();
-    const menu = document.getElementById('navbar-menu');
-    menu.classList.toggle('showMenu');
-  };
-
   return (
     <header>
       <nav className='navbar' role='navigation' aria-label='main navigation'>
         <div className='navbar-brand title is large'>
           <div className='barre'>
             <div className='totumtitle'>
-              <span onClick={onClick}>TOTUM</span>
+              <span onClick={backToHome}>TOTUM</span>
             </div>
             <div onClick={showMenu} className='icon' id='menu'>
               <FontAwesomeIcon icon={faUser} className='navbar-item ' />
@@ -61,7 +59,7 @@ function Header({ ...rest }) {
           <ul>
             <li onClick={profil}>Profile</li>
             <li onClick={setup}>Parametrès</li>
-            <li onClick={onClickLogout}>Déconnexion</li>
+            <li onClick={logout}>Déconnexion</li>
           </ul>
         </div>
       </div>
