c8034bb7616ee48c557d623f6229a9a169dca2c4
kenny caldieraro <kenny.caldieraro@gmail.com>
2022-06-18 20:34:09 +0200

    add conditional for add button

--- app/src/components/Footer/Footer.js
+++ app/src/components/Footer/Footer.js
@@ -1,22 +1,24 @@
-import React from 'react';
 import PropTypes from 'prop-types';
 import './Footer.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCirclePlus } from '@fortawesome/free-solid-svg-icons';
 import { useNavigate } from 'react-router-dom';
 
-function Footer({ ...rest }) {
+function Footer() {
   const navigate = useNavigate();
 
-  const onClick = async (event) => {
+  const createActivity = async (event) => {
     event.preventDefault();
-    navigate('/activity/create', { replace: true });
+    if (localStorage.getItem('id')) {
+      navigate('/activity/create', { replace: true });
+    } else {
+      navigate('/login', { replace: true });
+    }
   };
 
   return (
     <footer id='footer' className='iconbar'>
-      {/* <FontAwesomeIcon icon="fa-regular fa-circle-plus" /> */}
-      <button className='icon_create' onClick={onClick}>
+      <button className='icon_create' onClick={createActivity}>
         <FontAwesomeIcon
           icon={faCirclePlus}
           className='navbar-item icon is-large'
