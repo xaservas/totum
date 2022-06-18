046c5bc969eddc05c74f101678ddf6dffdf1da88
kenny caldieraro <kenny.caldieraro@gmail.com>
2022-06-18 20:32:30 +0200

    update joi schema

--- api/app/validation/schemas/activity/activityManage.schema.js
+++ api/app/validation/schemas/activity/activityManage.schema.js
@@ -1,6 +1,6 @@
 const Joi = require('joi');
 
-const textRule = Joi.string().min(2).required();
+const textRule = Joi.string().min(2);
 const numberRule = Joi.number().min(0).required();
 
 module.exports = Joi.object({
