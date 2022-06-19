a055ee88d620cad3d4785f70f0c458b441cc35ba
kenny caldieraro <kenny.caldieraro@gmail.com>
2022-06-19 12:36:25 +0200

    fix form

--- app/src/components/CreateProfil/CreateProfile.js
+++ app/src/components/CreateProfil/CreateProfile.js
@@ -1,20 +1,14 @@
-/* eslint-disable no-console */
 import React from 'react';
-
-// import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import './createProfile.scss';
-
-import axios from 'axios';
-// import PlacesAutocomplete from 'react-places-autocomplete';
+import axios from '../../utils/axiosPool';
 
 function CreateProfile() {
-  // const navigate = useNavigate();
-
+  const navigate = useNavigate();
   const [firstname, setFirstname] = React.useState('');
   const [lastname, setLastname] = React.useState('');
   const [email, setEmail] = React.useState('');
   const [password, setPassword] = React.useState('');
-
   const [passwordConfirmation, setPasswordConfirmation] = React.useState('');
   const [address, setAddress] = React.useState('');
   const [zipCode, setZipcode] = React.useState('');
@@ -35,17 +29,9 @@ function CreateProfile() {
     });
   }
 
-  const landmarkClick = () => {
-    const newValue = !landmarkValue;
-    setLandmarkValue(newValue);
-    console.log(newValue);
-  };
-
   const cookieClick = () => {
     const newValueB = !cookieValue;
     setCookieValue(newValueB);
-
-    console.log(newValueB);
   };
 
   const handleSubmit = (event) => {
@@ -68,30 +54,13 @@ function CreateProfile() {
         landmark: `${landmarkValue}`,
       },
     })
-      .then((response) => {
-        console.log(response);
-
-        console.log(response.data);
+      .then(() => {
+        navigate('/login', { replace: true });
       })
       .catch((error) => {
-        console.log(error);
+        // ajouter un message d'information si sa marche pas
+        throw new Error(error);
       });
-
-    console.log(`
-
-        Prénom : ${firstname}
-        Nom : ${lastname}
-        Email : ${email}
-        Mot de passe : ${password}
-        Adresse: ${address}
-        Code Postal: ${zipCode}
-        Ville : ${city}
-        Pays: ${country}
-        Présentation : ${about}
-        cookie:${cookieValue}
-        landmark: ${landmarkValue}
-        `);
-
     event.preventDefault();
   };
 
@@ -104,7 +73,6 @@ function CreateProfile() {
         placeholder='Prénom'
         onChange={(e) => setFirstname(e.target.value)}
       />
-
       <input
         name='lastname'
         type='text'
@@ -112,7 +80,6 @@ function CreateProfile() {
         placeholder='Nom'
         onChange={(e) => setLastname(e.target.value)}
       />
-
       <input
         name='email'
         type='email'
@@ -120,7 +87,6 @@ function CreateProfile() {
         placeholder='Mail'
         onChange={(e) => setEmail(e.target.value)}
       />
-
       <div className='password'>
         <input
           name='password'
@@ -129,16 +95,14 @@ function CreateProfile() {
           placeholder='Mot de passe'
           onChange={(e) => setPassword(e.target.value)}
         />
-
         <input
           name='passwordConfirmation'
           type='password'
           className='input'
           placeholder='Confirmation de Mot de passe'
           onChange={(e) => setPasswordConfirmation(e.target.value)}
-        />
+        />{' '}
       </div>
-
       <input
         name='address'
         type='text'
@@ -146,25 +110,22 @@ function CreateProfile() {
         placeholder='Adresse'
         onChange={(e) => setAddress(e.target.value)}
       />
-
       <div className='zipCity'>
         <input
-          name='zip_code'
+          name='zipCode'
           type='text'
           className='input'
           placeholder='Code Postal'
           onChange={(e) => setZipcode(e.target.value)}
         />
-
         <input
           name='city'
           type='text'
           className='input'
           placeholder='Ville'
           onChange={(e) => setCity(e.target.value)}
-        />
+        />{' '}
       </div>
-
       <input
         name='country'
         type='text'
@@ -172,29 +133,20 @@ function CreateProfile() {
         placeholder='Pays'
         onChange={(e) => setCountry(e.target.value)}
       />
-
       <input
         name='about'
         type='text'
-        className='textarea'
-        placeholder='Présente toi en quelques ligne'
+        className='input'
+        placeholder='Présentation'
         onChange={(e) => setAbout(e.target.value)}
       />
-
       <div className='OptionLogin'>
         <label className='checkbox'>
-          <input name='landmark' type='checkbox' onClick={landmarkClick} />
-          <span className='slider round'></span>
-          <p>Géolocalisation</p>
-        </label>
-        <label className='checkbox'>
-          <input name='cookie' type='checkbox' onClick={cookieClick} />
-          <span className='slider round'></span>
-          <p>Coockies</p>
+          <input name='cookie' type='checkbox' onClick={cookieClick} />{' '}
+          <span className='slider round'> </span> <p> cookies </p>{' '}
         </label>
       </div>
-
-      <button className='button'>Valider</button>
+      <button className='button'> Valider </button>{' '}
     </form>
   );
 }
