7bd75507999b48f8a1714c370453979dec7cd48d
Xavier Servas <xavier.servas@gmail.com>
2022-06-30 19:49:13 +0200

    apparence superposition modales

--- app/src/components/Error/Error.js
+++ app/src/components/Error/Error.js
@@ -4,7 +4,7 @@ function Error() {
   return (
     <div className='error'>
         <div className='viewport'>
-            <span className='message'>                Tu es PERDU-E....  Ceci est un écran blanc, dans une page 404, mais heureusement il y a un menu au dessus de ce message, tu peux t'en servir au lieu d'écrire n'importe quoi dans la barre d'URL!</span>
+            <span className='message'> Tu es PERDU-E visiblement....  Ceci est une page 404. Heureusement il y a un menu au dessus de ce message, tu peux t'en servir au lieu d'écrire n'importe quoi dans la barre d'URL!</span>
         </div>
     </div>
   );
