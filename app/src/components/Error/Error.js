5e50f9d0d5c457860a895bc5c85cebcc3bcb3239
kenny caldieraro <kenny.caldieraro@gmail.com>
2022-06-30 17:01:32 +0200

    add 404

---
+++ app/src/components/Error/Error.js
@@ -0,0 +1,12 @@
+import './error.scss';
+
+function Error() {
+  return (
+    <div className='error'>
+      <h1>Error 404</h1>
+      <p>Page not found</p>
+    </div>
+  );
+}
+
+export default Error;
